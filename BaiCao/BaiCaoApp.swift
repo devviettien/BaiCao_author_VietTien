07150295fb5be1fd8983890cf003c30edbdb5611
devviettien <devbuinhathoang@gmail.com>
2023-07-06 14:59:11 +0700

    Initial Commit

---
+++ BaiCao/BaiCaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaiCaoApp.swift
+//  BaiCao
+//
+//  Created by Nhật Hoàng on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BaiCaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
